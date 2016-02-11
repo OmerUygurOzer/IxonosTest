5f859bb66eeaf26a1867f55dd9ba63aa3a48572a
OmerUygurOzer <omer.u.ozer@gmail.com>
2016-02-10 19:23:08 -0800

    initialCommit

---
+++ app/src/main/java/com/boomer/omer/ixonostest/Splash.java
@@ -0,0 +1,22 @@
+package com.boomer.omer.ixonostest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Intent launch = new Intent(this,SignUp.class);
+        startActivity(launch);
+        finish();
+    }
+
+}
